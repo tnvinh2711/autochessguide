183f7dcb55b984aca800db5ce7beffec14bd0536
tnvinh <tnvinh2711@gmail.com>
2019-10-09 00:54:32 +0700

    add share function

--- AutoChessGuide/app/src/main/java/com/zinzin/tierbuilder/adapter/BuilderAdapter.java
+++ AutoChessGuide/app/src/main/java/com/zinzin/tierbuilder/adapter/BuilderAdapter.java
@@ -1,19 +1,28 @@
 package com.zinzin.tierbuilder.adapter;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.FileProvider;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.zinzin.tierbuilder.R;
 import com.zinzin.tierbuilder.model.Builder;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -44,7 +53,7 @@ public class BuilderAdapter extends RecyclerView.Adapter<BuilderAdapter.ViewHold
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder viewHolder, final int position) {
+    public void onBindViewHolder(@NonNull final ViewHolder viewHolder, final int position) {
         viewHolder.bind(builderList.get(position), position);
         final Builder builder = builderList.get(position);
         viewHolder.tvName.setText(builder.getName_team());
@@ -65,6 +74,34 @@ public class BuilderAdapter extends RecyclerView.Adapter<BuilderAdapter.ViewHold
                 listener.OnDeteleClick(position);
             }
         });
+        viewHolder.ivShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bitmap bitmap = createBitmapFromView(viewHolder.llCapture);
+                try {
+                    File cachePath = new File(activity.getCacheDir(), "images");
+                    cachePath.mkdirs(); // don't forget to make the directory
+                    FileOutputStream stream = new FileOutputStream(cachePath + "/image"+ builder.getName_team() + ".png"); // overwrites this image every time
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                    stream.close();
+                    File imagePath = new File(activity.getCacheDir(), "images");
+                    File newFile = new File(imagePath, "/image"+ builder.getName_team() + ".png");
+                    Uri contentUri = FileProvider.getUriForFile(activity, "com.zinzin.tierbuilder.fileprovider", newFile);
+
+                    if (contentUri != null) {
+                        Intent shareIntent = new Intent();
+                        shareIntent.setAction(Intent.ACTION_SEND);
+                        shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION); // temp permission for receiving app to read this file
+                        shareIntent.setDataAndType(contentUri, activity.getContentResolver().getType(contentUri));
+                        shareIntent.putExtra(Intent.EXTRA_STREAM, contentUri);
+                        activity.startActivity(Intent.createChooser(shareIntent, "Choose an app"));
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     @Override
@@ -76,13 +113,17 @@ public class BuilderAdapter extends RecyclerView.Adapter<BuilderAdapter.ViewHold
         TextView tvName, tvType;
         ImageView ivEdit;
         ImageView ivDelete;
+        ImageView ivShare;
         RecyclerView rcvUnit;
+        LinearLayout llCapture;
 
         public ViewHolder(View itemView) {
             super(itemView);
             ivEdit = itemView.findViewById(R.id.iv_edit);
             ivDelete = itemView.findViewById(R.id.iv_close);
             tvName = itemView.findViewById(R.id.tv_name);
+            ivShare = itemView.findViewById(R.id.iv_share);
+            llCapture = itemView.findViewById(R.id.ll_capture);
             tvType = itemView.findViewById(R.id.tv_type);
             rcvUnit = itemView.findViewById(R.id.rcv_unit);
         }
@@ -95,4 +136,20 @@ public class BuilderAdapter extends RecyclerView.Adapter<BuilderAdapter.ViewHold
         void OnEditClick(String name, int position);
         void OnDeteleClick(int position);
     }
+    private Bitmap createBitmapFromView(View v) {
+        v.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT));
+        v.measure(View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED),
+                View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
+        v.layout(0, 0, v.getMeasuredWidth(), v.getMeasuredHeight());
+        Bitmap bitmap = Bitmap.createBitmap(v.getMeasuredWidth(),
+                v.getMeasuredHeight(),
+                Bitmap.Config.ARGB_8888);
+
+        Canvas c = new Canvas(bitmap);
+        v.layout(v.getLeft(), v.getTop(), v.getRight(), v.getBottom());
+        v.draw(c);
+        return bitmap;
+    }
+
 }
